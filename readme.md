d4afd8e816446c0f794f1155122150a6589ca7d5
unknown <llucassilva088@gmail.com>
2023-12-28 12:37:25 -0400

    inicio do form

---
+++ readme.md
@@ -0,0 +1,9 @@
+<h1 align="center">Formulario de cadastro</h1>
+
+## Descrição do Projeto
+Foi desenvolvido um formulario simples de cadastro, para fins de estudo e pratica.
+
+## Tecnologias utilizadas
+. HTML
+. CSS
+. JavaScript
---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation")
+
+form.addEventListener("submit", (Event) =>{
+    Event.preventDefault();
+
+    checkInputusername();
+})
+
+// validar se campo de nome esta preenchido
+function checkInputusername(){
+    const usernameValue = username.value;
+
+    if(usernameValue === ""){
+        inputError(username, "Nome do usuário obrigatório.")
+    }else{
+        const formItem = username.parentElement;
+        formItem.classList = "form-content";
+    }
+}
+
+
+// informa menssagem de erro
+function inputError (input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a");
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error";
+}
